519c6f110921f2f7a501957e24edf801a4691a3b
denver <johndenver.diaz@umak.edu.ph>
2023-06-12 13:18:35 +0800

    Pricing

---
+++ src/lib/module/pricing/ui/list.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Ul from "$lib/component/list/ul.svelte";
+    import Li from "$lib/component/list/li.svelte";
+    export let data ,selectedItem;
+
+    const handleClick = (price) =>(selectedItem = price)
+
+</script>
+
+<Ul>
+    {#each data as data}
+      <Li>
+        <div class="d-flex align-items-center justify-content-between">
+            <button type="button" class="list-group-item list-group-item-action" on:click={()=>handleClick(data.price)}>
+                {data.name}- {data.price}
+            </button>
+        </div>
+      </Li>
+    {/each}
+  </Ul>
